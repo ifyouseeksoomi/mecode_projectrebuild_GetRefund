e9a12cb1fb5c964101f5502dae311b7da7afd565
soomialexhwang <ifyouseeksoomi@gmail.com>
2020-08-19 22:34:41 +0900

    [SOOME] superfluid 초기세팅 완료

---
+++ my_settings.py
@@ -0,0 +1,15 @@
+SECRET_KEY = {
+    'secret' : '%q4m0wt*80i*jmw^h#44x+(t_%d^qf3dr3p*sfw_0#n18fara%'
+}
+    
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'get_refund2',
+        'USER': 'root',
+        'PASSWORD': 'mysqlHard123^^',
+        'HOST': 'project-database2.cvfnwctpsjzo.us-east-2.rds.amazonaws.com',
+        'PORT': '3306',
+    }
+}
+
